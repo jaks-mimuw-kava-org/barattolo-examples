c21293882f285f54ad8700f06d29f4dfd339515f
Michal Wisniewski <michal.wisniewski@allegro.pl>
2023-01-19 10:12:25 +0100

    main- | Update example.

--- postgres/init.sql
+++ postgres/init.sql
@@ -4,10 +4,16 @@ CREATE DATABASE test;
 
 \c test;        
 
+CREATE TABLE House(
+    address VARCHAR PRIMARY KEY,
+    area INTEGER
+);
+
 CREATE TABLE Person(
     id SERIAL PRIMARY KEY,
     first varchar(50),
     last varchar(50),
     age INTEGER,
-    phonenumber char(9)
+    phonenumber char(9),
+    house VARCHAR
 );
